6ba04d206747b004bf7ebf76b14b654ddd83c8c4
Sone <sthaya@gmail.com>
2020-07-20 10:53:40 -0700

    changed end point

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
 
   useEffect(() => {
-    axios.get('https://cors-anywhere.herokuapp.com/https://fortnite-server-api.herokuapp.com/api/store', {
+    axios.get('https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/store', {
       headers: {
         'TRN-Api-Key': process.env.REACT_APP_API_KEY
         
